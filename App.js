c18d8ae4c74b9ea2eeeccd49219c2a66a44571f1
BiriBiri2077 <braxyychantop2019@gmail.com>
2023-09-19 20:24:26 -0300

    update

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text style={{fontSize:22, textAlign:'center', paddingTop:20, backgroundColor:'grey',paddingBottom:5}}>pudim bobao</Text>
       <StatusBar style="auto" />
+      <Image source={{uri: 'https://rlv.zcache.com.br/relogio_grande_sorrindo_o_macaco-rf6b6ef91230943a6a423bb593437c552_s0ysp_8byvr_644.jpg'}}
+       style={{width: 400, height: 400}} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
