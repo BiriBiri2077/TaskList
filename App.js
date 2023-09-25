c79460742c21af30391a2411b748c752bcf98adc
BiriBiri2077 <braxyychantop2019@gmail.com>
2023-09-25 20:02:59 -0300

    up to date

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View,ImageBackground, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
   
   const image = require('./resources/bg.jpg');
+
+  console.disableYellowBox = true;
   
+  const [tarefas, setarTarefas] = useState([
+    {
+      id: 1,
+      tarefa: 'Minha tarefa 1.'
+    },
+    {
+      id:2,
+      tarefa: 'Minha tarefa 2.'
+    }
+  ]);
   
+  function deletarTarefa(id){
+    alert('Tarefa com id'+id+' foi deletada com sucesso!');
+    let newTarefas = tarefas.filter(function(val){
+          return val.id != id;
+    });
+
+    setarTarefas(newTarefas);
+  }
+  
+
+
   return (
     <View style={{flex:1}}>
       <ImageBackground source={image} style={styles.image}>
@@ -13,6 +37,23 @@ export default function App() {
         <Text style={styles.textHeader}>Lista de tarefas</Text>
         </View>
       </ImageBackground>
+
+
+
+    {
+      tarefas.map(function(val){
+    return (<View style={styles.tarefaSingle}>
+      <View style={{flex:1,width:'100%', padding:10}}>
+        <Text>{val.tarefa}</Text>
+      </View>
+      <View style={{alignItems:'flex-end', flex:1, paddingEnd:10}}>
+        <TouchableOpacity onPress={()=> deletarTarefa(val.id)}><Text style={{fontSize:25}}>+</Text></TouchableOpacity>
+      </View>
+    </View>);
+    })
+    }
+
+
     </View>
   );
 }
@@ -34,5 +75,11 @@ const styles = StyleSheet.create({
     color:'white',
     fontSize:20,
     marginTop:20
+  },
+  tarefaSingle:{
+    marginTop:30,
+    width:'100%',
+    flexDirection:'row',
+    paddingBottom:10
   }
 });
