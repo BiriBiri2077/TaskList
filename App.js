102e60a070e5528f72f11608add1f5e260bea66b
BiriBiri2077 <braxyychantop2019@gmail.com>
2023-10-02 20:15:13 -0300

    update 434

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View,ImageBackground, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground, TextInput, TouchableOpacity, TouchableHighlight,SafeAreaView, Modal } from 'react-native';
 
 
 export default function App() {
@@ -7,6 +7,8 @@ export default function App() {
   const image = require('./resources/bg.jpg');
 
   console.disableYellowBox = true;
+
+  const [tarefaAtual,setTaskAtual] = useState('');
   
   const [tarefas, setarTarefas] = useState([
     {
@@ -18,6 +20,10 @@ export default function App() {
       tarefa: 'Minha tarefa 2.'
     }
   ]);
+
+  const [modal,setModal] = useState(false);
+
+ 
   
   function deletarTarefa(id){
     alert('Tarefa com id'+id+' foi deletada com sucesso!');
@@ -27,11 +33,48 @@ export default function App() {
 
     setarTarefas(newTarefas);
   }
+
+  function addTarefa(){
+      setModal(!modal);
+      let id = 0;
+      if(tarefas.length > 0){
+        id = tarefas[tarefas.length-1].id+1;
+      }
+
+      let tarefa = {id:id,tarefa:tarefaAtual};
+
+      setarTarefas([...tarefas,tarefa]);
+
+  }
+
   
 
 
   return (
     <View style={{flex:1}}>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput onChangeText={text => setTaskAtual(text)} autoFocus={true}></TextInput>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => addTarefa()}
+            >
+              <Text style={styles.textStyle}>Adicionar Tarefa</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
       <ImageBackground source={image} style={styles.image}>
       <View style={styles.coverView}>
         <Text style={styles.textHeader}>Lista de tarefas</Text>
@@ -52,6 +95,8 @@ export default function App() {
     </View>);
     })
     }
+
+    <TouchableOpacity style={styles.bynAddTarefa} onPress={()=>setModal(true)}><Text style={{textAlign:'center', color:'white'}}>Adicionar Tarefa!</Text></TouchableOpacity>
 
 
     </View>
@@ -80,6 +125,52 @@ const styles = StyleSheet.create({
     marginTop:30,
     width:'100%',
     flexDirection:'row',
-    paddingBottom:10
+    paddingBottom:10,
+  },
+  //Estilos para nossa modal
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:'rgba(0,0,0,0.5)'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex:5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  bynAddTarefa: {
+    width:200,
+    padding:8,
+    backgroundColor:'gray',
+    marginTop:20
   }
+
+ 
 });
